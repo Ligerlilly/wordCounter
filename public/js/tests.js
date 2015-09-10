08712f22b2c89f15fd59785d57506354b03507eb
Jason Lane & Andrew Brennwald <student@epicodus.com>
2015-09-10 14:16:28 -0700

    Add return word count

--- public/js/tests.js
+++ public/js/tests.js
@@ -2,7 +2,7 @@ var wordOrder = require('./word_order');
 var expect = require('chai').expect;
 
 describe("wordOrder", function() {
-  it("returns true", function() {
-    expect(wordOrder()).to.eq(true);
+  it("returns world: 2 hello: 1 for input hello world world", function() {
+    expect(wordOrder("hello hello world world world")).to.eql([ 'world: 3', 'hello: 2' ]);
   });
 });
--- public/js/word_order.js
+++ public/js/word_order.js
@@ -1,5 +1,48 @@
-var wordOrder = function () {
-  return false;
+var wordOrder = function (phrase) {
+  var phraseWords = phrase.split(' ');
+  var sortable = [];
+  var foundWords = {};
+  var formattedWords = [];
+
+
+
+  // Creates new objects for each word.
+  // Increments count of word if encountered again
+
+  for (var i in phraseWords) {
+    foundWordsArray = Object.keys(foundWords);
+    if (typeof foundWordsArray[0] == 'undefined' ) {
+      foundWordsArray[0]  = null;
+    }
+
+    for (var j in foundWordsArray) {
+      if (phraseWords[i] == foundWordsArray[j]) {
+        foundWords[foundWordsArray[j]] += 1;
+      }
+      else if (foundWords[phraseWords[i]] == null) {
+        foundWords[phraseWords[i]] = 1;
+      }
+    }
+  }
+
+  // Pushed each object into an array to be sorted
+
+  for (var word in foundWords){
+    sortable.push([word, foundWords[word]])
+  }
+
+  // Sorts the array of word / count objects in descending order
+
+  sortable.sort(function(a,b){return b[1] - a[1]});
+
+  // Converts each word / count object into a formatted string.
+  // Pushes converted strings into a new array, and returns the array.
+
+  for (var i = 0; i < sortable.length; i++) {
+    formattedWords.push(sortable[i][0] +  ': ' + sortable[i][1]);
+  }
+  console.log(foundWords);
+  return formattedWords;
 };
 
 module.exports = wordOrder;
